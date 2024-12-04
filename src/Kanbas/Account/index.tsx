b05f843617b60c4593720670d843271cd6646805
Param Joshi <param.joshi@enthralltech.com>
2024-12-03 19:47:38 -0500

    Restored deleted files

--- src/Kanbas/Account/index.tsx
+++ src/Kanbas/Account/index.tsx
@@ -2,35 +2,6 @@ import Profile from "./Profile";
 import Signin from "./Signin";
 import { Routes, Route, Navigate } from "react-router";
 import Signup from "./Signup";
-<<<<<<< Updated upstream
-import AccountNavigation from "./Navigation";
-export default function Account() {
-  return (
-    <div id="wd-account-screen">
-      <h2>Account</h2>
-      <h3> Name :- Param Rajesh Joshi</h3>
-      <h4> Section: 2</h4>
-      <table>
-        <tr>
-          <td valign="top">
-            <AccountNavigation />
-          </td>
-          <td valign="top">
-      <Routes>
-        <Route path="/" element={<Navigate to="/Kanbas/Account/Signin" />} />
-        <Route path="/Signin" element={<Signin />} />
-        <Route path="/Profile" element={<Profile />} />
-        <Route path="/Signup" element={<Signup />} />
-      </Routes>
-      </td>
-        </tr>
-      </table>
-
-    </div>
-  );
-}
-
-=======
 import { useSelector } from "react-redux";
 import Users from "./Users";
 export default function Account() {
@@ -58,4 +29,3 @@ export default function Account() {
       </div>
   );}
   
->>>>>>> Stashed changes
