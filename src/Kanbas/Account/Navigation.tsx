86a95d9b683b32fe5271bf73848ee4216ea032ef
Param Joshi <param.joshi@enthralltech.com>
2024-12-03 19:38:16 -0500

    third commit

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -1,13 +1,37 @@
+<<<<<<< Updated upstream
 import { Link } from "react-router-dom";
+=======
+import { Link, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
+export default function AccountNavigation() {
+    const { pathname } = useLocation();
+    const active = (path: string) => (pathname.includes(path) ? "active" : "");
+    const acc_links = ["Signin", "Signup", "Profile"];
+    const { currentUser } = useSelector((state: any) => state.accountReducer);
+    const links = currentUser ? ["Profile"] : ["Signin", "Signup"]
+>>>>>>> Stashed changes
 
 export default function AccountNavigation()
 {
     return (
+<<<<<<< Updated upstream
         <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
             <Link to={'/Kanbas/Account/Signin'} className="list-group-item active border border-0">Signin</Link>          
             <Link to={'/Kanbas/Account/Signup'} className="list-group-item text-danger border border-0">Signup</Link>   
             <Link to={'/Kanbas/Account/Profile'} className="list-group-item text-danger  border border-0">Profile</Link>                   
             
+=======
+        <div id="wd-account-navigation" style={{marginTop:"10px"}} className="wd list-group fs-5 rounded-0">
+            {acc_links.map((acc_link) => (
+                <Link key={`/Kanbas/Account/${acc_link}`} to={`/Kanbas/Account/${acc_link}`} className={`list-group-item border border-0
+                ${pathname.includes(acc_link) ? "active" : "text-danger"}`}>
+                    {acc_link}
+                </Link>
+            ))}
+            {currentUser && currentUser.role === "ADMIN" && (
+                <Link to={`/Kanbas/Account/Users`} className={`list-group-item border-0 border-white ${active("Users")}`}> Users </Link>
+            )}
+>>>>>>> Stashed changes
         </div>
 
 
--- src/Kanbas/Account/index.tsx
+++ src/Kanbas/Account/index.tsx
@@ -2,6 +2,7 @@ import Profile from "./Profile";
 import Signin from "./Signin";
 import { Routes, Route, Navigate } from "react-router";
 import Signup from "./Signup";
+<<<<<<< Updated upstream
 import AccountNavigation from "./Navigation";
 export default function Account() {
   return (
@@ -29,3 +30,32 @@ export default function Account() {
   );
 }
 
+=======
+import { useSelector } from "react-redux";
+import Users from "./Users";
+export default function Account() {
+    const { currentUser } = useSelector((state: any) => state.accountReducer);
+    return (
+      <div id="wd-account-screen">
+        <table>
+          <tr>
+            <td valign="top">
+              <AccountNavigation />
+            </td>
+            <td valign="top">
+              <Routes>
+                <Route path="/"
+                       element={<Navigate to={ currentUser ? "/Kanbas/Account/Profile" : "Kanbas/Account/Signin" }/>} />
+                <Route path="/Signin" element={<Signin />} />
+                <Route path="/Profile" element={<Profile />} />
+                <Route path="/Signup" element={<Signup />} />
+                <Route path="/Users" element={<Users />}/>
+                <Route path="/Users/:uid" element={<Users />}/>
+              </Routes>
+            </td>
+          </tr>
+        </table>
+      </div>
+  );}
+  
+>>>>>>> Stashed changes
