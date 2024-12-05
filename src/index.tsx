d50b2513cfc0003fc923335d5eaeb5a6545e9574
Param Joshi <param.joshi@enthralltech.com>
2024-12-04 20:10:30 -0500

    second commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+// import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+reportWebVitals();
